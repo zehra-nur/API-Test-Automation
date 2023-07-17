0b02d494790ed57c85596a147b5a23b8e2ea056b
zehranur <zehranuravcibasi@gmail.com>
2023-07-17 11:36:10 +0300

    Response Body Assertion

---
+++ src/test/java/C07_Get_ResponseBodyTesti.java
@@ -0,0 +1,41 @@
+import io.restassured.http.ContentType;
+import io.restassured.response.Response;
+import org.hamcrest.Matchers;
+import org.junit.Test;
+
+import static io.restassured.RestAssured.given;
+
+public class C07_Get_ResponseBodyTesti {
+
+    /*
+        https://jsonplaceholder.typicode.com/posts/44 url'ine bir GET request yolladigimizda
+        donen Response’in
+            status code'unun 200,
+            ve content type'inin Aplication.JSON,
+            ve response body'sinde bulunan userId'nin 5,
+            ve response body'sinde bulunan title'in "optio dolor molestias sit"
+            oldugunu test edin.
+     */
+
+    @Test
+    public void test01(){
+
+        // 1- Request body ve end-point hazirlama
+
+        String url = "https://jsonplaceholder.typicode.com/posts/44";
+
+        // 2- Expected data hazirlama
+
+        // 3- Request gonderip, donen response'i kaydetme
+
+        Response response = given().when().get(url);
+
+        // 4- Assertion
+
+        response
+                .then().assertThat().statusCode(200)
+                                    .contentType(ContentType.JSON)
+                                    .body("userId", Matchers.equalTo(5))
+                                    .body("title",Matchers.equalTo("optio dolor molestias sit"));
+    }
+}
