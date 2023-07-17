219f3bd9e6f849d418002eb3fbcc8b32c8369d00
zehranur <zehranuravcibasi@gmail.com>
2023-07-17 10:39:26 +0300

    JSON objesi olusturma

---
+++ src/test/java/C04_JsonObjesiOlusturma.java
@@ -0,0 +1,24 @@
+import org.json.JSONObject;
+import org.junit.Test;
+
+public class C04_JsonObjesiOlusturma {
+
+    @Test
+    public void test01(){
+        /*
+             {
+             "title":"Ahmet",
+             "body":"Merhaba",
+             "userId":1
+             }
+         */
+
+        JSONObject jsonObject = new JSONObject();
+
+        jsonObject.put("title","Ahmet");
+        jsonObject.put("body","Merhaba");
+        jsonObject.put("userId",1);
+
+        System.out.println(jsonObject);
+    }
+}
